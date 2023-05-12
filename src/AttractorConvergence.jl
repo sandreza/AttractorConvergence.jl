b96fde21d6eea9ae5631487ada20d01797cf40d5
Andre Souza <andrenogueirasouza@gmail.com>
2023-05-12 17:39:29 -0400

    indexing

--- src/AttractorConvergence.jl
+++ src/AttractorConvergence.jl
@@ -19,7 +19,6 @@ function lorenz(x, ρ, σ, β)
     return [σ * (x2 - x1), x1 * (ρ - x3) - x2, x1 * x2 - β * x3]
 end
 
-
 export RungeKutta4, rk4
 
 function rk4(f, s, dt)
@@ -59,7 +58,7 @@ function (step::RungeKutta4)(f, x, dt)
     return nothing
 end
 
-export StateEmbedding
+export StateEmbedding, StateTreeEmbedding
 
 abstract type AbstractEmbedding end
 
@@ -70,5 +69,25 @@ function (embedding::StateEmbedding)(current_state)
     argmin([norm(current_state - markov_state) for markov_state in embedding.markov_states])
 end
 
+struct StateTreeEmbedding{S, T} <: AbstractEmbedding
+    markov_states::S
+    levels::T
+end
+function (embedding::StateTreeEmbedding)(current_state)
+    global_index = 1 
+    for level in 1:embedding.levels
+        new_index = argmin([norm(current_state - markov_state) for markov_state in embedding.markov_states[global_index]])
+        global_index = child_global_index(new_index, global_index)
+    end
+    return local_index(global_index, embedding.levels)
+end
 
+# assumes binary tree
+local_index(global_index, levels) = global_index - 2^levels + 1 # markov index from [1, 2^levels]
+# parent local index is markov_index(global_index, levels-1)
+# child local index is 2*markov_index(global_index, levels-1) + new_index - 1
+# global index is 2^levels + 1 + child local index
+child_global_index(new_index, global_parent_index, level) = (2 * (local_index(global_parent_index, level - 1)-1) + new_index - 1) + 2^(level) 
+# simplified:
+child_global_index(new_index, global_parent_index) = 2 * global_parent_index + new_index - 1 
 end # module AttractorConvergence
