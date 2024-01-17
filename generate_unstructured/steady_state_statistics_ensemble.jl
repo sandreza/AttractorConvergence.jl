0750c5a62df345c9950d9dd47b7d7d02fcb18d1f
sandreza <andrenogueirasouza@gmail.com>
2024-01-17 15:42:20 -0500

    steady_state_statistics computations

---
+++ generate_unstructured/steady_state_statistics_ensemble.jl
@@ -0,0 +1,42 @@
+using HDF5, ProgressBars
+
+data_directory = "/real_data"
+hfile = h5open(pwd() * data_directory  * "/ensemble_mean_statistics.hdf5", "w")
+centers_hfile = h5open(pwd() * data_directory  * "/centers.hdf5", "r")
+eigenvalues_hfile = h5open(pwd() * data_directory  * "/eigenvalues.hdf5", "r")
+imax = length(keys(centers_hfile)) - 1
+for i in ProgressBar(1:imax)
+    centers = read(centers_hfile["centers $i"])
+    p = read(eigenvalues_hfile["generator steady state $i"] )
+    hfile["number of partitions $i"] = length(p)
+    for (jj, state_string) in ProgressBar(enumerate(["x", "y", "z"]))
+        observables = [i -> i[jj]^j for j in 1:5]
+        partition_number = size(centers)[2]
+        ensemble_mean = zeros(length(observables))
+        for (oo,observable) in enumerate(observables)
+            for ii in 1:partition_number
+                ensemble_mean[oo] += observable(centers[:, ii]) .* p[ii]
+            end
+        end
+        hfile[state_string * " moments $i"] = ensemble_mean
+    end
+end
+close(centers_hfile)
+close(eigenvalues_hfile)
+close(hfile)
+
+##
+hfile = h5open(pwd() * data_directory  * "/ensemble_mean_statistics.hdf5", "r+")
+for i in ProgressBar(1:imax)
+    for state_string in ["x", "y", "z"]
+        moments = read(hfile[state_string * " moments $i"])
+        cumulants_list = similar(moments)
+        cumulants_list[1] = moments[1]
+        cumulants_list[2] = moments[2] - moments[1]^2
+        cumulants_list[3] = moments[3] - 3 * moments[1] * moments[2] + 2 * moments[1]^3
+        cumulants_list[4] = moments[4] - 4 * moments[1] * moments[3] - 3 * moments[2]^2 + 12 * moments[1]^2 * moments[2] - 6 * moments[1]^4
+        cumulants_list[5] = moments[5] - 5 * moments[1] * moments[4] - 10 * moments[2] * moments[3] + 20 * moments[1]^2 * moments[3] + 30 * moments[1] * moments[2]^2 - 60 * moments[1]^3 * moments[2] + 24 * moments[1]^5
+        hfile[state_string * " cumulants $i"] = cumulants_list
+    end
+end
+close(hfile)
---
+++ generate_unstructured/steady_state_statistics_timeseries.jl
@@ -0,0 +1,40 @@
+data_directory = "/real_data"
+
+dt_skip = 0.01
+
+@info "loading data"
+hfile = h5open(pwd() * data_directory  * "/lorenz.hdf5", "r")
+dt = read(hfile["dt"])
+skip = round(Int, dt_skip/dt)
+m_timeseries = read(hfile["timeseries"])[:, 1:skip:end]
+s_timeseries = read(hfile["symmetrized timeseries"])[:, 1:skip:end]
+joined_timeseries = hcat(m_timeseries, s_timeseries) # only for Partitioning Purpose
+close(hfile)
+
+
+hfile = h5open(pwd() * data_directory  * "/time_mean_statistics.hdf5", "w")
+hfile["skip"] = skip
+hfile["dt_skip"] = dt_skip
+for (jj, state_string) in ProgressBar(enumerate(["x", "y", "z"]))
+    observables = [i -> i[jj]^j for j in 1:5]
+    @info "computing observables from timeseries"
+    observables_list = zeros(length(observables))
+    for j in ProgressBar(eachindex(observables))
+        N = length(eachcol(joined_timeseries))
+        for state in ProgressBar(eachcol(joined_timeseries))
+            observables_list[j] += observables[j](state) / N
+        end
+    end
+
+
+    cumulants_list = similar(observables_list)
+    cumulants_list[1] = observables_list[1]
+    cumulants_list[2] = observables_list[2] - observables_list[1]^2
+    cumulants_list[3] = observables_list[3] - 3 * observables_list[1] * observables_list[2] + 2 * observables_list[1]^3
+    cumulants_list[4] = observables_list[4] - 4 * observables_list[1] * observables_list[3] - 3 * observables_list[2]^2 + 12 * observables_list[1]^2 * observables_list[2] - 6 * observables_list[1]^4
+    cumulants_list[5] = observables_list[5] - 5 * observables_list[1] * observables_list[4] - 10 * observables_list[2] * observables_list[3] + 20 * observables_list[1]^2 * observables_list[3] + 30 * observables_list[1] * observables_list[2]^2 - 60 * observables_list[1]^3 * observables_list[2] + 24 * observables_list[1]^5
+
+    hfile[state_string * " moments"] = observables_list[1:5]
+    hfile[state_string * " cumulants"] = cumulants_list[1:5]
+end
+close(hfile)
