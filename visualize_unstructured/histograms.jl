02bbf49eec611e85492354e6b16aca9957006cc0
sandreza <andrenogueirasouza@gmail.com>
2024-01-17 17:37:52 -0500

    udpate to include kl divergence

--- visualize_unstructured/histograms.jl
+++ visualize_unstructured/histograms.jl
@@ -60,3 +60,20 @@ end
 display(fig)
 
 save(pwd() * figure_directory * "/histogram_convergence.png", fig)
+
+##
+function kl_divergence(p, q)
+    kldiv = 0.0
+    for i in eachindex(p)
+        if p[i] > 0.0
+            kldiv += p[i] * log(p[i] / q[i])
+        end
+    end
+    return kldiv
+end
+
+npartitions = [length(ps[i]) for i in 1:25]
+kldivs = [kl_divergence(probabilities_e[i], probabilities_t[i]) for i in 1:25]
+x = log10.(npartitions)
+y = log10.(kldivs)
+scatter(x, y)
